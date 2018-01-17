46246f19fee7c6523d89a1d3808c367772365d35
Vimal <lalanivimalr@gmail.com>
2018-01-17 19:13:13 +0530

    Initial commit

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularCRUD.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        AngularCRUD.Models.EmployeeDBEntities EmpDB = new Models.EmployeeDBEntities();
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetAllEmployee()
+        {            
+            return Json(EmpDB.Employees.ToList(),JsonRequestBehavior.AllowGet);
+        }
+        public string DeleteEmployee(Models.Employee emp)
+        {
+            EmpDB.Employees.Remove(EmpDB.Employees.Where(p=>p.Emp_Id == emp.Emp_Id).First());
+            EmpDB.SaveChanges();
+            return "Record Deleted Sucessfully !!";
+        }
+        public string AddEmployee(Models.Employee emp)
+        {
+            EmpDB.Employees.Add(new Models.Employee() { Emp_Age = emp.Emp_Age ,Emp_City=emp.Emp_City,Emp_Name=emp.Emp_Name});
+            EmpDB.SaveChanges();
+            return "Record Added Sucessfully !!";
+        }
+    }
+}
---
+++ Scripts/AngularCore.js
@@ -0,0 +1,51 @@
+﻿var app = angular.module("myApp", []);
+app.controller("myCtrl", function ($scope, $http) {
+    $scope.Employee = {};
+    $scope.Employee.Emp_Name = "";
+    $scope.Employee.Emp_Age = "";
+    $scope.Employee.Emp_City = "";
+    $scope.GetEmployeeData = function () {
+        $http({
+            method: "get",
+            url: "http://localhost:52155/Employee/GetAllEmployee"
+        }).then(function (response) {
+            $scope.employees = response.data;
+        }, function (response) {
+            alert("Error Occur", response.data);
+        });
+    }
+    $scope.delete = function (emp) {
+        $http({
+            method: "post",
+            url: "http://localhost:52155/Employee/DeleteEmployee",
+            datatype: "json",
+            data: JSON.stringify(emp)
+        }).then(function (response) {
+            $http({
+                method: "get",
+                url: "http://localhost:52155/Employee/GetAllEmployee"
+            }).then(function (response) {
+                $scope.employees = response.data;
+            }, function (response) {
+                alert("Error Occur", response.data);
+            });
+        
+            alert(response.data);
+        }, function (response) {
+            alert("Error Occur", response.data);
+        });        
+    }
+    $scope.Add = function () {
+        $http({
+            method: "post",
+            url: "http://localhost:52155/Employee/AddEmployee",
+            datatype: "json",
+            data: JSON.stringify($scope.Employee)
+        }).then(function (response) {
+            $scope.GetEmployeeData();
+            alert(response.data);
+        }, function (response) {
+            alert("Error Occur", response.data);
+        });
+    }
+});
